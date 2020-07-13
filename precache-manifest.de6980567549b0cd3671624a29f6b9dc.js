29ea37c0b6da3182678891cbac36ce158f66436b
Andrew Weiss <wandrew8@gmail.com>
2020-07-13 10:15:17 -0700

    Updates

---
+++ precache-manifest.de6980567549b0cd3671624a29f6b9dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbebaa1976f9308707933934b5f3d50",
+    "url": "/bulma-theming/index.html"
+  },
+  {
+    "revision": "199e08b84a9bff6d14b4",
+    "url": "/bulma-theming/static/css/main.468557f3.chunk.css"
+  },
+  {
+    "revision": "55c89debfd481116dff4",
+    "url": "/bulma-theming/static/js/2.c49a4309.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bulma-theming/static/js/2.c49a4309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199e08b84a9bff6d14b4",
+    "url": "/bulma-theming/static/js/main.e459356a.chunk.js"
+  },
+  {
+    "revision": "73f02b9d812cf2a0bdbd",
+    "url": "/bulma-theming/static/js/runtime-main.8ae352c0.js"
+  }
+]);
